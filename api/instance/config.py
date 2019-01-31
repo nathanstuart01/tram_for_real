77eb8c452795cbbc8ff29e7776260a193d51dd33
Nathan Stuart <nathanstuart@Jessicas-MBP.domain>
2019-01-31 05:53:23 -0700

    added database url to config.py, flask-variables.env, added migration script to add users table to tram db

--- api/instance/config.py
+++ api/instance/config.py
@@ -5,7 +5,7 @@ class Config(object):
     """ Parent Congifuration Class """
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:password@db:5432/tram'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
