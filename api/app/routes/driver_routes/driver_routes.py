e21d5a138698a3d3f42ae1f7f2d5e1b52f477bbe
Nathan Stuart <nathan.stuart@ascentialedge.com>
2019-09-23 16:50:14 -0600

    added update and show driver details routes, same for riders routes

--- api/app/routes/driver_routes/driver_routes.py
+++ api/app/routes/driver_routes/driver_routes.py
@@ -40,3 +40,34 @@ def show_driver_trips():
         return jsonify({'Message': 'unable to return selected driver trips'}), 500
 
 #curl -i -X GET -H "Authorization: Bearer $ACCESS" -H "Content-Type: application/json" http://localhost:5000/api/show_driver_trips
+
+@app.route('/api/show_driver', methods=['GET'])
+@jwt_required
+def show_driver_info():
+    current_user_info = get_jwt_claims()
+    driver_id = current_user_info['user_id']
+    try:
+        driver = Driver.query.filter(Driver.user_id == driver_id).first()
+        return jsonify({'driver first name': driver.first_name, 'driver last name': driver.last_name, 'driver id': driver.id}), 200
+    except Exception as e:
+        return jsonify({'error': e.args}), 500
+
+#curl -i -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $ACCESS" http://localhost:5000/api/show_driver
+
+@app.route('/api/update_driver/<int:driver_id>', methods=['PUT'])
+@jwt_required
+def update_driver(driver_id):
+    driver = Driver.query.filter(Driver.id == driver_id).first()
+    driver_first_name = request.json.get('driver_first_name')
+    driver_last_name = request.json.get('driver_last_name')
+    if driver_first_name:
+        driver.first_name = driver_first_name
+    if driver_last_name:
+        driver.last_name = driver_last_name
+    db.session.add(driver)
+    db.session.commit()
+    return jsonify({'updated_driver_info': {'updated_driver_first_name': driver.first_name,
+                                          'updated_driver_last_name': driver.last_name,
+                                            }
+                    })
+#curl -i -X PUT -H "Authorization: Bearer $ACCESS" -H "Content-Type: application/json" -d '{"driver_first_name":"testy little updated driver first name", "driver_last_name": "testy little updated driver last name"}' http://localhost:5000/api/update_driver/1
