b9b99f3f6ba9cbeee7cdee0844cc069cc18d11e5
Nathan Stuart <nathan.stuart@ascentialedge.com>
2020-01-03 10:43:46 -0700

    updated driver component to have props instead of state

--- frontend/src/components/DriverTrips.js
+++ frontend/src/components/DriverTrips.js
@@ -1,37 +1,34 @@
 import React from 'react';
-import { BASE_URL } from './Url';
 
-const endpoint = 'show_driver_trips';
+class DriverTrips extends React.Component {
 
-const handleTrips = (trips) => {
-    var trips = trips['Current active driver trips'];
-    var i;
-    for (i = 0; i < trips.length; i++) {
-        console.log(trips[i]);
+    constructor(props) {
+        super(props);
     }
-}
 
-const getDriverTrips = (token) => {
-    fetch(`${BASE_URL}/${endpoint}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-        }).then(res => res.json())
-            .then( result => {
-                handleTrips(result)
+        render() {
+            let trips = this.props.driverTrips.map( (trip) => {
+                return (
+                    <ul key={trip.trip_id}>
+                        <li>
+                            <h6>Trip Date: {trip.departure_date}</h6>
+                        </li>
+                    </ul>
+                );
             })
-            .catch( error => {
-                console.log(error.message);
-            })
+            if(trips.length !==0) {
+                return (
+                    <div>
+                        <h3>My Trips As Driver:</h3>
+                        {trips}
+                    </div>
+                );
+            } else {
+                return (
+                    <h5>You Haven't Created Any Rides</h5>
+                );
+            }
         }
-
-const DriverTrips = () => {
-
-    return (
-    <div><button onClick={() => getDriverTrips(localStorage.getItem('access_token'))}>My Driver Trips</button></div>
-    );
-}
+    }   
 
 export default DriverTrips;
--- frontend/src/components/userHomePage.js
+++ frontend/src/components/userHomePage.js
@@ -4,13 +4,41 @@ import { logout } from './Auth';
 import { authenticatedUser } from './Auth';
 import DriverTrips  from './DriverTrips';
 import RiderTrips from './RiderTrips';
+import { BASE_URL } from './Url';
 
 class UserHomePage extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { trips: []};
+      }
+
+    getDriverTrips = (token) => {
+        fetch(`${BASE_URL}/${'show_driver_trips'}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+            }).then(res => res.json())
+                .then( result => {
+                    this.setState({trips: result['Current active driver trips']});
+                })
+                .catch( error => {
+                    console.log(error.message);
+                })
+            }
+    
+    componentDidMount() {
+        var token = localStorage.getItem('access_token');
+        this.getDriverTrips(token);
+    }
+    
     render(){
+
         return (
             <div>Authenticated User Home Page
-                <div id='driverTrips'><DriverTrips /></div>
+                <DriverTrips driverTrips={this.state.trips} />
                 <div id='riderTrips'><RiderTrips /></div>
                 <button onClick={ (history) => {logout(); this.props.history.push('/')}}>Logout</button>
             </div>
