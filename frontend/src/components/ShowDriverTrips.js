6251f614d5625fe57c3e550300d43a00c5f66044
Nathan Stuart <nathanstuart@Natalies-MBP.domain>
2020-01-15 21:21:20 -0700

    added intial button to edit a trip

--- frontend/src/components/ShowDriverTrips.js
+++ frontend/src/components/ShowDriverTrips.js
@@ -7,7 +7,12 @@ class ShowDriverTrips extends React.Component {
     componentDidMount() {
     }
 
+    toggleEdit = () => {
+        this.setState({ edit: !this.state.edit });
+    }
+
     edit = () => {
+        let trip = this.state
         return (
             <div>Edit Driver Trip</div>
         )
@@ -15,12 +20,18 @@ class ShowDriverTrips extends React.Component {
 
     show = () => {
         let trip = this.props.location.state.trip;
-        console.log(trip);
         if(Object.keys(trip).length) {
             return (
                 <div>
+                    <div><button onClick={ () => {this.toggleEdit()}}>Edit This Trip</button></div>
+                    <hr />
                 <h1>Trip Details</h1>
-                <h4>{trip.departure_date}</h4>
+                <h4>Departure Date: {trip.departure_date}</h4>
+                <h4>Return Date: {trip.return_date}</h4>
+                <h4>Start Location: {trip.start_location}</h4>
+                <h4>End Location: {trip.end_location}</h4>
+                <h4>Driver ID: {trip.driver_id}</h4>
+                <h4>Trip ID: {trip.trip_id}</h4>
                 </div>
             )
         }
